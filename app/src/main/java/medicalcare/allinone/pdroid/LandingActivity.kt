d55b639a79dfeddec7f154f3120735db6173f6c6
sushantrijal123 <sushant.rijal@amokshoneeighty.com>
2023-02-13 14:43:37 +0530

    LandingActivity Created

---
+++ app/src/main/java/medicalcare/allinone/pdroid/LandingActivity.kt
@@ -0,0 +1,12 @@
+package medicalcare.allinone.pdroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LandingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
